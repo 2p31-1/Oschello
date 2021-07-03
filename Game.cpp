2ce02b7e6494336f9613cce953e3bddd3d78ce32
2147483647 <hyeonsoft@gmail.com>
2021-07-03 22:13:37 +0900

    Update Game.cpp

--- Game.cpp
+++ Game.cpp
@@ -4,26 +4,33 @@
 
 using namespace std;
 
-class Board {
-	
+class Game {
+	/*
+	gameMode
+	bit	black	white
+	00	player	player
+	01	player	AI
+	10	AI		Player
+	11	AI		AI
+	*/
+	int gameMode;
+	Board playboard();
+public:
+	Game() {}
+	Game(int gameMode) { this->gameMode = gameMode; }
+	void play() {
+
+	}
+
+	void init() {
+		cout << "Oschello, the AI Othello game.\n";
+		cout << "Put your game mode";
+		gameMode = UI::getInt() % 4;
+	}
 };
-
-class UI {
-
-};
-
-class AI {
-
-};
-
-void play() {
-
-}
-
-void init() {
-
-}
 
 void main() {
-
+	Game othello{};
+	othello.init();
+	othello.play();
 }
